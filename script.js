094204157938c61b5d54e441c6aa2aea91dd0e28
fbgrigolo <fbc.grigolo@gmail.com>
2022-08-03 19:58:55 -0300

    Fixing some errors

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-import Coutdown from './countdown.js';
+import Coutdown from './modules/countdown.js';
 
-timeForChristimas = new Coutdown(' 24 December 2022 23:49:59');
-timeForNewYear = new Coutdown(' 31 December 2022 23:49:59 GMT-0300');
+let timeForChristimas = new Coutdown(' 24 December 2022 23:49:59');
+let timeForNewYear = new Coutdown(' 31 December 2022 23:49:59 GMT-0300');
 
 setInterval(() => {
-  console.log(timeForNewYear.total);
+	console.log(timeForChristimas.total, 'Time for christimas');
+	// console.log(timeForNewYear.total, 'Time for new year');
 }, 1000);
